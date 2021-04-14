9b665651785df120323d62b38485c634f1f7b031
Danil <danilstasilevich@yandex.ru>
2021-04-15 05:33:36 +0700

    Добавил награды

---
+++ Reward/Reward.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace CarGameScripts.Reward
+{
+    [Serializable]
+    public class Reward
+    {
+        public RewardType RewardType;
+        public Sprite IconCurrency;
+        public int CountCurrency;
+    }
+}
